4214ab179e7182c45c396d049ccd8b460586218f
Skay <skaytech30@gmail.com>
2020-08-11 16:06:46 +0530

    Updated comments

--- js/app.js
+++ js/app.js
@@ -1,20 +1,36 @@
+//Get the Element with the Id 'root'
 const rootDiv = document.getElementById('root');
+
+//Declare the variables for home, about & contact html pages
 let home = '';
 let about = '';
 let contact = '';
 
-// Load the HTML Page based on page passed in as 'param'
+/**
+ *
+ * @param {String} page - Represents the page information that needs to be retrieved
+ * @returns {String} resHtml - The Page's HTML is returned from the async invocation
+ */
+
 const loadPage = async (page) => {
-  const res = await (await fetch(page)).text();
-  return res;
+  const response = await fetch(page);
+  const resHtml = await response.text();
+  return resHtml;
 };
 
+/**
+ * The Async function loads all HTML to the variables 'home', 'about' & 'contact'
+ */
 const loadAllPages = async () => {
   home = await loadPage('home.html');
   about = await loadPage('about.html');
   contact = await loadPage('contact.html');
 };
 
+/**
+ * The Main Function is an async function that first loads All Page HTML to the variables
+ * Once the variables are loaded with the contents, then they are assigned to the 'routes' variable
+ */
 const main = async () => {
   await loadAllPages();
   rootDiv.innerHTML = home;
@@ -25,13 +41,23 @@ const main = async () => {
   };
 };
 
+// Invoke the Main function
 main();
 
+/**
+ *
+ * @param {String} pathname - Pass the 'pathname' passed from onClick function of the link (index.html)
+ * The function is invoked when any link is clicked in the html.
+ * The onClick event on the html invokes the onNavClick & passes the pathname as param
+ */
 const onNavClick = (pathname) => {
   window.history.pushState({}, pathname, window.location.origin + pathname);
   rootDiv.innerHTML = routes[pathname];
 };
 
+/**
+ * The Function is invoked when the window.history's state changes
+ */
 window.onpopstate = () => {
   rootDiv.innerHTML = routes[window.location.pathname];
 };
